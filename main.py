d48d70ede8aba8f98049eca04bdfc305fe02865c
Marc <marc.friedrich94@gmail.com>
2019-04-30 10:39:45 +0200

    added decorator to register permutation handler

--- main.py
+++ main.py
@@ -18,40 +18,55 @@ import argparse
 
 # pip installation über py binary: py -m pip install nltk
 
-parser = argparse.ArgumentParser(description="Password hash anaylsis using WordNet and the HaveIBeenPwned database")
-parser.add_argument("-p", "--pass-database", type=str, required=True, help="Path to the HIBP password database", dest="pass_db_path")
-parser.add_argument("-d", "--depth", type=int, required=True, help="Depth in the DAG", dest="dag_depth")
+parser = argparse.ArgumentParser(
+    description="Password hash anaylsis using WordNet and the HaveIBeenPwned database")
+parser.add_argument("-p", "--pass-database", type=str, required=True,
+                    help="Path to the HIBP password database", dest="pass_db_path")
+parser.add_argument("-d", "--depth", type=int, required=True,
+                    help="Depth in the DAG", dest="dag_depth")
 args = parser.parse_args()
 
 total_processed = 0
 started = ""
 outfile_name = "results.txt"
 outfile_f = open(outfile_name, "w+")
-
-def lookup_password():
-    pwned_pass_list = "E:\\HIBP Passwords\\pwned-passwords-sha1-ordered-by-count-v4.txt"
-    hashed_pw = hashlib.sha1("test".encode("utf-8")).hexdigest()
-    
+permut_handler = None
 
 
 def sigint_handler(sig, frame):
+    """Register the handler for the SIGINT signal.
+
+    This is absolutely necessary to exit the program with Ctrl+C because a user easily misconfigure the 
+    programe (i.e. -d > 4) for it to result in a combinatorial explosion because of its recursion.
+    """
     print()
     print("Caught Ctrl+C, shutting down...")
     sys.exit(0)
 
 
 def get_shell_width():
+    """Return the number of colums in the current shell view.
+    """
     cols, _ = shutil.get_terminal_size((80, 20))
     return cols
 
 
 def get_curr_time():
+    """Return the current time, nicely formatted as a string.
+    """
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
 def clear_terminal():
-    os.system("clear") if platform.system() == "Linux" or platform.system() == "Darwin" else os.system("cls")
+    """Clear the terminal. This is required to properly display the stats while running.
+    """
+    os.system("clear") if platform.system(
+    ) == "Linux" or platform.system() == "Darwin" else os.system("cls")
+
 
 def update_stats(current, finished):
+    """Print out the stats while the program is running.
+    """
     clear_terminal()
     print()
     print()
@@ -65,33 +80,31 @@ def update_stats(current, finished):
     print()
 
 
-
 def lookup_pass(hash):
-    """Wrapper for _lookup_in_hash_file
+    """Wrapper for _lookup_in_hash_file. Returns the occurrences of the 
+    searched hash/password in the HIBP password file.
     """
     occurrences = _lookup_in_hash_file(hash)
     if occurrences is None:
         return 0
-    else: 
+    else:
         return int(occurrences.split(":")[1])
 
 
 def _lookup_in_hash_file(hash):
+    """Implements actual file access.
+    """
     try:
-        result = subprocess.check_output(["look", "-f", hash, args.pass_db_path])
+        result = subprocess.check_output(
+            ["look", "-f", hash, args.pass_db_path])
     except CalledProcessError as e:
         return None
     return result.decode("utf-8").strip("\n").strip("\r")
-        
 
-
-def analyze_root_hypernyms(loops=100):
-    synsets = list(wn.all_synsets("n"))[100:100+loops]
-    for syn in synsets:
-        hyper_path = [s.name() for s in syn.hypernym_paths()[0]]
-        print("  %s path: %s" % (syn.name(), hyper_path))
 
 def hash_sha1(s):
+    """Hash the password.
+    """
     return hashlib.sha1(s.encode("utf-8")).hexdigest()
 
 
@@ -104,7 +117,7 @@ def recurse_nouns_from_root(root_syn, max_depth=0):
     Each indented set of lemmas is the sum of all unpacked lemmas of each synset of the current graph level.
     """
     if root_syn.min_depth() == max_depth:
-            return
+        return
     curr_root_syn = root_syn
     for hypo in curr_root_syn.hyponyms():
         for lemma in hypo.lemma_names():
@@ -113,15 +126,35 @@ def recurse_nouns_from_root(root_syn, max_depth=0):
             global total_processed
             total_processed += 1
             _write_result_to_results_file(lemma, hypo.min_depth(), occurrences)
-            update_stats(current="%s / %d" % (lemma, occurrences), finished=total_processed)
+            update_stats(current="%s / %d" %
+                         (lemma, occurrences), finished=total_processed)
         recurse_nouns_from_root(root_syn=hypo, max_depth=max_depth)
 
+
+def _permut_registrar():
+    permut_registry = []
+
+    def registrar(func):
+        permut_registry.append(func)
+        return func
+    registrar.all = permut_registry
+    return registrar
+
+
+permut_handler = _permut_registrar()
+
+
+@permut_handler
+def some_permut_handler1(s):
+    print("!! permut_handler1 exec !! %s" % s)
 
 
 def _write_result_to_results_file(lemma_name, lemma_depth, occurrences):
     """Wrapper for the _write_results_file() function
     """
-    _write_to_results_file("%s%s %d" % (lemma_depth * "  ", lemma_name, occurrences))
+    _write_to_results_file("%s%s %d" % (
+        lemma_depth * "  ", lemma_name, occurrences))
+
 
 def _write_to_results_file(s):
     outfile_f.write("%s\n" % s)
@@ -135,12 +168,18 @@ if __name__ == "__main__":
     print()
     started_time = get_curr_time()
 
+    # print("handler: %s" % permut_handler.all)
+    # for h in permut_handler.all:
+    #     h("testString")
+
     root_syn = wn.synset("entity.n.01")
     for root_lemma in root_syn.lemma_names():
-        _write_result_to_results_file(root_lemma, root_syn.min_depth(), lookup_pass(hash_sha1(root_lemma)))
+        _write_result_to_results_file(
+            root_lemma, root_syn.min_depth(), lookup_pass(hash_sha1(root_lemma)))
         total_processed += 1
-    
+
     recurse_nouns_from_root(root_syn=root_syn, max_depth=args.dag_depth)
+
     print()
     _write_to_results_file("")
     _write_to_results_file(40 * "=")
@@ -152,6 +191,4 @@ if __name__ == "__main__":
     print()
     print("  Results written to %s" % outfile_name)
     outfile_f.close()
-    
-
     print()
